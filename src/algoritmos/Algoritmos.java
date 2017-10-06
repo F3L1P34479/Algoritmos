701aedfc75ec667694d7b8ce801b86d0340f7ab2
Aluno <Aluno@172.32.1.13>
2017-10-05 23:58:10 -0300

    Criação da função metade e alteração para funções com retorno

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -1,20 +1,35 @@
 package algoritmos;
 
+import java.util.Scanner;
+
 
 public class Algoritmos {
 
-    public static void quadrado(double n){
-        
+    public static double quadrado(double n){
         double r=n*n;
+        return r;
         
     }
-    public static void main(String[] args) {
+    
+    public static double metade(double n){
         
+        double r=n/2;
+        return r;
+    }
+    
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
         //calculando raiz quadrada
         double raiz;
         raiz=Math.sqrt(15);
         
         System.out.println("A raiz é "+raiz);
+        
+        System.out.print("Informe um numero:");
+        double n=e.nextDouble();
+        
+        System.out.println("O quadrado do valor informado é "+quadrado(n));
+        System.out.println("A metade do valor informado é "+metade(n));
     }
     
 }
